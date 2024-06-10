dc710872dab973c89b233b439e72c61e465f6a41
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2024-06-09 18:45:44 -0600

    📝 add inscriptions data

--- src/main/java/dois/webapp/resources/RecursoInscripciones.java
+++ src/main/java/dois/webapp/resources/RecursoInscripciones.java
@@ -1,10 +1,12 @@
 package dois.webapp.resources;
 
 import dois.webapp.entidades.Inscripciones;
+import dois.webapp.entidades.Materia;
 import dois.webapp.negocio.DataService;
 import jakarta.ejb.EJB;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -23,5 +25,11 @@ public class RecursoInscripciones {
     public Response getInscripciones() {
         List<Inscripciones> inscripciones = servicio.getInscripciones();
         return Response.ok(inscripciones).build();
+    }
+    
+    @POST
+    public Response saveInscripciones(Inscripciones inscripciones) {
+        servicio.saveInscripcion(inscripciones);
+        return Response.ok("Inscripcion creada exitosamente").build();
     }
 }
